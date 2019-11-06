c5d6035caa6e4b493cd016d3b99ffab1a1a6fc41
EorEquis <eeyore@stuffupthere.com>
2019-11-06 08:48:27 -0600

    Initial Commit

---
+++ RR4005i Switch/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ASCOM RR4005i.Switch")> 
+<Assembly: AssemblyDescription("ASCOM Switch driver for RR4005i")> 
+<Assembly: AssemblyCompany("ASCOM Initiative")> 
+<Assembly: AssemblyProduct("ASCOM Switch driver for RR4005i")> 
+<Assembly: AssemblyCopyright("Copyright © 2019, The ASCOM Intiative")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9850c0bf-b719-4697-b31c-1a908e193929")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("6.4.0.0")>
+<Assembly: AssemblyFileVersion("6.4.0.0")>
